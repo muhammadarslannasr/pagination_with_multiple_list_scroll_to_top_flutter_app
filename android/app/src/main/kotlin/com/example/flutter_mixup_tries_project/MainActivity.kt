c1a98435e49fc266ae4fc09f894c15a6d8ce4f39
Arslan <arslan.nasr@rnssol.com>
2023-12-06 16:54:41 +0500

    init first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_mixup_tries_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_mixup_tries_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/multiple_list_pagination_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class MultipleListPaginationScreen extends StatefulWidget {
+  const MultipleListPaginationScreen({super.key});
+
+  @override
+  State<MultipleListPaginationScreen> createState() =>
+      _MultipleListPaginationScreenState();
+}
+
+class _MultipleListPaginationScreenState
+    extends State<MultipleListPaginationScreen> {
+  List<String> itemList1 = [];
+  List<String> itemList2 = [];
+
+  int currentPage1 = 1;
+  int currentPage2 = 1;
+
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize data for the first page of each list
+    itemList1 = fetchDataForPage(currentPage1);
+    itemList2 = fetchDataForPage(currentPage2);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  void _loadMoreData(int listNumber) {
+    // Fetch more data based on the current page for the specified list
+    if (listNumber == 1) {
+      setState(() {
+        itemList1.addAll(fetchDataForPage(++currentPage1));
+      });
+    } else if (listNumber == 2) {
+      setState(() {
+        itemList2.addAll(fetchDataForPage(++currentPage2));
+      });
+    }
+  }
+
+  List<String> fetchDataForPage(int page) {
+    // Simulate fetching data from an API or other source
+    return List.generate(10, (index) => 'Item ${index + (page - 1) * 10}');
+  }
+
+  Widget buildLoadMoreButton(int listNumber) {
+    return ElevatedButton(
+      onPressed: () {
+        _loadMoreData(listNumber);
+      },
+      child: const Text('Load More'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dual ListViews with Load More'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.keyboard_arrow_up),
+        onPressed: () {
+          // Scroll to the top when the button is pressed
+          _scrollController.animateTo(
+            0.0,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+          );
+        },
+      ),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: itemList1.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(itemList1[index]),
+                  // Other list item configurations
+                );
+              },
+            ),
+            buildLoadMoreButton(1),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: itemList2.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(itemList2[index]),
+                  // Other list item configurations
+                );
+              },
+            ),
+            buildLoadMoreButton(2),
+          ],
+        ),
+      ),
+    );
+  }
+}
